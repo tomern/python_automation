5ff80b2ba77a96191cf8bffd070fb5bc938901c5
Tomer Noy <tomernoy1@gmail.com>
2018-10-08 00:14:38 +0300

    using tuples in sql select

--- tests_folder/test_sql.py
+++ tests_folder/test_sql.py
@@ -1,10 +1,14 @@
 import pyodbc
 
+pyodbc.pooling = False
+
 
 def test_sql(app_config):
     conn = pyodbc.connect(app_config.connections['sql_local'])
     cursor = conn.cursor()
-    cursor.execute("SELECT FirstName as name FROM [Northwind].[dbo].[Employees] WHERE EmployeeID = 7")
-    row = cursor.fetchone()
+    cursor.execute("SELECT FirstName, LastName "
+                   "FROM [Northwind].[dbo].[Employees] "
+                   "WHERE EmployeeID = 7")
+    first_name, last_name = cursor.fetchone()
     conn.close()
-    assert 'Robert' == row[0]
+    assert first_name == 'Robert' and last_name == 'King'
