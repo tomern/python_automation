e7b215e23f02883f48aaa97f1c947afbe12d3f11
Tomer Noy <tomernoy1@gmail.com>
2018-10-09 02:20:22 +0300

    removing sleep from tests

--- tests_folder/test_pytest_selenium.py
+++ tests_folder/test_pytest_selenium.py
@@ -3,10 +3,10 @@ from time import sleep
 
 def test_example1(selenium):
     selenium.get('http://www.example.com')
-    sleep(15)
+    # sleep(15)
 
 
 def test_example2(selenium):
     selenium.get('http://www.example.com')
-    sleep(15)
+    # sleep(15)
     assert 2 == 1
