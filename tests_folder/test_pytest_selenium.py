27fffde3482a9575c888d56ee6237d29a36b6880
Tomer Noy <tomernoy1@gmail.com>
2018-10-10 22:33:48 +0300

    adding print for testing real time status on jenkins console output

--- tests_folder/test_pytest_selenium.py
+++ tests_folder/test_pytest_selenium.py
@@ -3,10 +3,14 @@ from time import sleep
 
 def test_example1(selenium):
     selenium.get('http://www.example.com')
-    # sleep(15)
+    for i in range(10):
+        print('test_example1 step{0}'.format(i))
+        sleep(1)
 
 
 def test_example2(selenium):
     selenium.get('http://www.example.com')
-    # sleep(15)
+    for i in range(10):
+        print('test_example2 step{0}'.format(i))
+        sleep(1)
     assert 2 == 1
