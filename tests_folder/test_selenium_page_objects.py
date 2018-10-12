57f617ac63aae2b4f993fb4b85ed1ea2e5ca3e3a
Tomer Noy <tomernoy1@gmail.com>
2018-10-12 11:22:14 +0300

    adding tests with page-objects library

--- tests_folder/test_selenium_page_objects.py
+++ tests_folder/test_selenium_page_objects.py
@@ -1,9 +1,12 @@
-from pages.login_page import GooglePage
-from time import sleep
+from pages.my_pages_objects.james_allen_footer_object import JamesAllenFooter
+# from pages.my_pages_objects.james_signup_thanks_popup_object import JamesSignupThanksPopup
 
 
-def test_test_page_objects(selenium):
-    google_page = GooglePage(selenium, root_uri='https://www.google.com/')
-    google_page.get('')
-    sleep(5)
-    google_page.gmail_btn.click()
+def test_test_page_objects(selenium, app_config):
+    selenium.get(app_config.urls['jamesallen_url'])
+    james_allen_footer = JamesAllenFooter(selenium)
+    james_allen_footer.newsletter_input = 'asdfasdf'
+    # james_allen_footer.signup_email_list(app_config.user_jamesallen)
+    # james_signup_thanks_popup = JamesSignupThanksPopup(selenium)
+    # james_signup_thanks_popup.close_register_popup()
+    # assert james_signup_thanks_popup.check_popup_disabled()
